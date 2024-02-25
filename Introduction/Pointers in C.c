0fc1f0ff0e3486667e2436cd6652c256d1562d42
Doli <67073431+Dolyetyus@users.noreply.github.com>
2024-02-25 17:38:57 +0100

    Create Pointers in C.c

---
+++ Introduction/Pointers in C.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    int temp = *a;
+    int temp2 = *b;
+    
+    *a = temp + temp2;
+    *b = abs(temp - temp2);
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
