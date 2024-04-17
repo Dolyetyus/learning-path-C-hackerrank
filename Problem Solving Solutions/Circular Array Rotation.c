e2a771f8a7a5eca025d98c0fb1ff95ba2c1e2e15
Doli <67073431+Dolyetyus@users.noreply.github.com>
2024-04-17 17:18:11 +0200

    Update Circular Array Rotation.c

--- Problem Solving Solutions/Circular Array Rotation.c
+++ Problem Solving Solutions/Circular Array Rotation.c
@@ -1,13 +1,12 @@
 int* circularArrayRotation(int a_count, int* a, int k, int queries_count, int* queries, int* result_count) {
-    int* result = malloc(queries_count * sizeof(int));
     k = k % a_count;
     
     for (int i = 0; i < queries_count; i++) {
         int index = (queries[i] - k + a_count) % a_count;
-        result[i] = a[index];
+        queries[i] = a[index];
     }
 
     *result_count = queries_count;
 
-    return result;
+    return queries;
 }
